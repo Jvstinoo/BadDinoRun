89528d0b445407e6f20927e6f38d0dc7d2616e1d
Jvstinoo <justin.balbuena@upr.edu>
2022-07-24 21:37:33 -0700

    initial commit: added basic logic

---
+++ Block.js
@@ -0,0 +1,41 @@
+class Block {
+    constructor(canv, ctx) {
+        this.canv = canv;
+        this.ctx = ctx
+        this.x = canv.width * .1;
+        this.y = canv.height
+        this.width = 30;
+        this.height = 30;
+        this.vel = 8;
+        this.jumping = false;
+        this.score = 0;
+        this.highScore = 0;
+    }
+
+    move() {
+
+        addEventListener("keydown", (event) => {
+            if (event.key == ' ' && this.y == this.canv.height) {
+                this.jumping = true;
+            }
+        })
+        // Add jumping code here
+        if (this.jumping) {
+            this.y -= this.vel;
+            if (this.y <= this.canv.height - 120) {
+                this.jumping = false;
+            }
+        } else if (this.y != this.canv.height) {
+            this.y += 4;
+        }
+    }
+    draw() {
+        ctx.fillStyle = 'green';
+        ctx.fillRect(this.x, this.y - this.height, this.width, this.height);
+    }
+    main() // Will run all methods in the class.
+    {
+        this.move();
+        this.draw();
+    }
+}
---
+++ script.js
@@ -0,0 +1,59 @@
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const goat = document.getElementById('goat');
+let timer = 0;
+
+const player = new Block(canvas, ctx);
+const enemy = new EnemyBlock(canvas, ctx);
+
+window.onload = function () {
+    document.addEventListener("keydown", (event) => {
+        if (event.key == ' ' && enemy.vel == 0) {
+            console.log("Game started");
+            enemy.vel = 6;
+            enemy.x = canvas.width;
+            player.vel = 8;
+            if (player.score > player.highScore)
+                player.highScore = player.score;
+            player.score = 0;
+        }
+    })
+}
+
+function drawScore() {
+    if (timer % 60 == 0 && timer != 0)
+        player.score++;
+    ctx.fillStyle = 'white';
+    ctx.fillText("Score: " + player.score, canvas.width - 100, 20);
+    ctx.fillText("High Score: " + player.highScore, canvas.width - 100, 40);
+}
+
+function pressSpace() {
+    if (enemy.vel == 0) {
+        ctx.fillStyle = 'white';
+        ctx.fillText("Press Space to start", canvas.width / 2 - 50, canvas.height / 2);
+    }
+}
+
+function collision() {
+    // Check collision with enemy
+    if (player.x + player.width > enemy.x && player.x < enemy.x + enemy.width && player.y + player.height > enemy.y && player.y < enemy.y + enemy.height) {
+        player.vel = 0;
+        enemy.vel = 0;
+    } else { enemy.vel > 0 ? timer++ : timer = 0; }
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawScore();
+    pressSpace();
+    player.main();
+    enemy.main();
+    collision();
+}
+
+
+// Make draw run every frame
+setInterval(draw, 1000 / 60);
